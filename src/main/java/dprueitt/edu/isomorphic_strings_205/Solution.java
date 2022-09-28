5b1ba69cc8ece76ad442a7e5d9d32545330e858b
Daniel Stephen Prueitt <danielprueitt@gmail.com>
2022-09-28 21:26:10 +0900

    Complete 205_Isomorphic_Strings

--- src/main/java/dprueitt/edu/isomorphic_strings_205/Solution.java
+++ src/main/java/dprueitt/edu/isomorphic_strings_205/Solution.java
@@ -1,2 +1,46 @@
-package dprueitt.edu.isomorphic_strings_205;public class Solution {
+package dprueitt.edu.isomorphic_strings_205;
+
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution
+{
+    public boolean isIsomorphic( String s, String t )
+    {
+        if( s == null || t == null )
+        {
+            return false;
+        }
+
+        final Map<Character, Character> mapping_s_t = new HashMap<>();
+        final Map<Character, Character> mapping_t_s = new HashMap<>();
+
+        for( int i = 0; i < s.length(); i++ )
+        {
+            final char sChar = s.charAt( i );
+            final char tChar = t.charAt( i );
+            final Character sTLookup = mapping_s_t.get( sChar );
+
+            if( sTLookup == null )
+            {
+                mapping_s_t.put( sChar, tChar );
+            }
+            else if( sTLookup != tChar )
+            {
+                return false;
+            }
+
+            final Character tSLookup = mapping_t_s.get( tChar );
+            if( tSLookup == null )
+            {
+                mapping_t_s.put( tChar, sChar );
+            }
+            else if( tSLookup != sChar )
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
 }
--- src/test/java/dprueitt/edu/isomorphic_strings_205/SolutionTest.java
+++ src/test/java/dprueitt/edu/isomorphic_strings_205/SolutionTest.java
@@ -1,12 +1,28 @@
 package dprueitt.edu.isomorphic_strings_205;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.stream.Stream;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class SolutionTest
 {
 
-    @org.junit.jupiter.api.Test
-    void isIsomorphic()
+    private static Stream<Arguments> isIsomorphic()
     {
+        return Stream.of(
+                Arguments.of( "egg", "add", true ),
+                Arguments.of( "foo", "bar", false ),
+                Arguments.of( "paper", "title", true )
+        );
+    }
+
+
+    @ParameterizedTest
+    @MethodSource
+    void isIsomorphic( String s, String t, boolean expected )
+    {
+        assertEquals( expected, new Solution().isIsomorphic( s, t ) );
     }
 }
